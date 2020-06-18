0718b136d060567135c037eeac12216dcabfc09e
Krystian <krystianw.e.t@gmail.com>
2020-06-18 21:58:50 +0200

    fixed requests vol2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO USER_TABLE (ID, USER_LOGIN, FULLNAME, SECOND_NAME, COUNTRY, CITY, POINTS)
-VALUES (user_seq.nextval, 'armn', 'NeilArmstrong','SECOND NAME','XD','xd',10);
+VALUES (user_seq.nextval, 'Kanekx', 'fullname1','seconaname1','country1','city1',10);
 INSERT INTO QUEST (ID, FULLNAME, CITY, STATUS, USER_ID)
-VALUES (quest_seq.nextval, 'aadam','xd','COMPLETED',1);
+VALUES (quest_seq.nextval, 'Adam','Kowalski','COMPLETED',1);
 
